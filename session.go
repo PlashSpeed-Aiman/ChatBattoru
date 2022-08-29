d6a096ef9ce1201cc3d022028e4f13f7da873cbd
AIMAN RAHIM <maiman.ar@live.iium.edu.my>
2022-08-30 00:17:50 +0800

    started writing on Session.go

--- session.go
+++ session.go
@@ -1,2 +1,16 @@
 // session logic goes here
 package main
+
+type Session struct {
+	CurrentGameState GameState
+	SessionId        string
+}
+
+//I'm not sure of the program flow yet. This is just for reference
+func StartNewSession() Session {
+	var s Session
+	s.CurrentGameState.tic_tac_state = make([]int, 9)
+	s.CurrentGameState.current_turn = 0
+	//s.SessionId = panic("Some Random Number")
+	return s
+}
